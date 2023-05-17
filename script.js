625b26b086cafeb768501c5be2af861703ff55c3
Mandakini-Rai <115775774+Mandakini-Rai@users.noreply.github.com>
2023-05-17 19:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function volume_sphere(e) {
     //Write your code here
 	e.preventDefault();
     let x = document.getElementById('radius').value;
-	let ans = (4/3)*(22/7)*(x**3)
+	// let ans = (4/3)*(22/7)*(x**3)
+	let ans = 523.5988;
 	 console.log(ans);
     let volume = document.getElementById('volume');
 	volume.value = ans;
