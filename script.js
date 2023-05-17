9451ec4a228af039af3722c336c4de539ec66981
Mandakini-Rai <115775774+Mandakini-Rai@users.noreply.github.com>
2023-05-17 18:53:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-	
+	e.preventDefault();
     let x = document.getElementById('radius').value;
 	let ans = (4*22*x**3)/7;
 	 console.log(ans);
     let volume = document.getElementById('volume');
 	volume.value = ans;
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
